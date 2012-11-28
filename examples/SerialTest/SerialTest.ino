a0104d9bf1aec16059422e3042e751ddfecc9bf4
Jonathan Oxer <jon@oxer.com.au>
2012-11-28 20:26:48 +1100

    Reinstated SerialTest example

---
+++ examples/SerialTest/SerialTest.ino
@@ -0,0 +1,22 @@
+/*
+ * File:    SerialTest.ino
+ * Version: 1.0
+ * Author:  Andy Gelme (@geekscape)
+ * License: GPLv3
+ */
+
+#include <SPI.h>
+#include "Cube.h"
+
+Cube cube;
+
+void setup(void) {
+#if defined(ARDUINO) && ARDUINO >= 101
+  while (Serial == false) ;                 // Wait for Leonardo USB serial port
+#endif
+
+  cube.begin(0, 115200);                    // Port: Serial, Baudrate: 115200
+}
+
+void loop(void) {
+}
